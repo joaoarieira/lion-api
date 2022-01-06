0cfd5ccb02c8261c34debd5839febaa3a27af6e2
João Azevedo <joao.azevedo@ufv.br>
2022-01-06 01:34:53 -0300

    update-user.dto changed

--- src/http/users/dto/update-user.dto.ts
+++ src/http/users/dto/update-user.dto.ts
@@ -1,4 +1,31 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  IsBoolean,
+  IsEmail,
+  IsOptional,
+  IsString,
+  MinLength,
+  Validate,
+} from 'class-validator';
+import { IsAvaible } from 'src/validators/is-avaible';
+import { User } from '../entities/user.entity';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsEmail()
+  @Validate(IsAvaible, [User, 'email'])
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @IsOptional()
+  @IsBoolean()
+  @IsOptional()
+  is_active?: boolean;
+}
